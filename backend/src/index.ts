6c2318d6ceb15c505df9754b71c50a90a18696ee
Karan <karansharma192004@gmail.com>
2025-02-02 21:46:15 +0530

    Basic chat functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,32 @@
-import { WebSocketServer } from "ws"
+import WebSocket, { WebSocketServer } from "ws"
 
-const wss = new WebSocketServer({port:8080})
+const wss = new WebSocketServer({port:8080},()=>
+console.log("server listening at port 8080"))
 
+interface Users{
+    socket: WebSocket,
+    username: string,
+    roomId: string
+}
 
-wss.on("connection", (ws)=>{
-    ws.on("message", (message)=>{
+let allUsers: Users[] = []
+
+wss.on("connection", (socket)=>{
+    socket.on("message", (message)=>{
         const parsedMessage = JSON.parse(message as unknown as string)
-        ws.send("pong")
-        ws.send(parsedMessage.message)
+        if(parsedMessage.type === "JOIN_ROOM"){
+            allUsers.push({
+                socket,
+                username: parsedMessage.payload.username,
+                roomId: parsedMessage.payload.roomId
+
+            })
+        }
+
+        if(parsedMessage.type === "CHAT"){
+            const user = allUsers.find((u) => u.socket == socket);
+            const rooms = allUsers.filter(u => u.roomId == user?.roomId)
+            rooms.map(room => room.socket.send(parsedMessage.payload.message))
+        }
     })
 })
